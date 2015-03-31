b6412c31d08317b7b5eacee91618f3fede363999
Konstantin Stepanov <me@kstep.me>
2015-03-31 18:06:33 +0300

    factor out process

--- src/main.rs
+++ src/main.rs
@@ -5,40 +5,17 @@
 #[macro_use(log)]
 extern crate cronparse;
 
-use std::convert::AsRef;
-use std::fs::{walk_dir, PathExt};
-use std::path::{Path, PathBuf};
 use std::thread::spawn;
-use std::collections::BTreeMap;
 use std::env;
 
-use cronparse::{CrontabFile, CrontabFileError, CrontabFileErrorKind};
-use cronparse::crontab::{UserCrontabEntry, SystemCrontabEntry, AnacrontabEntry, EnvVarEntry, CrontabEntry, ToCrontabEntry};
-use cronparse::log::{KernelLogger, ConsoleLogger, AnyLogger, Logger, LogLevel};
+use cronparse::crontab::{UserCrontabEntry, SystemCrontabEntry, AnacrontabEntry};
+use cronparse::log::{KernelLogger, ConsoleLogger, AnyLogger};
 
-fn generate_systemd_units(path: &Path, entry: CrontabEntry, env: &BTreeMap<String, String>) {
-    println!("{} => {:?}, {:?}", path.display(), entry, env);
-}
+mod process;
 
 static USERS_CRONTAB_DIR: &'static str = "/var/spool/cron";  // UserCrontabEntry
 static SYSTEM_CRONTAB_DIR: &'static str = "/etc/cron.d";  // SystemCrontabEntry
 static ANACRONTAB_FILE: &'static str = "/etc/anacrontab";  // AnacrontabEntry
-
-fn process_crontab_file<T: ToCrontabEntry, P: AsRef<Path>>(path: P, logger: &mut Logger) {
-    CrontabFile::<T>::new(path.as_ref()).map(|crontab| {
-        let mut env = BTreeMap::new();
-        for entry in crontab {
-            match entry {
-                Ok(CrontabEntry::EnvVar(EnvVarEntry(name, value))) => { env.insert(name, value); },
-                Ok(data) => generate_systemd_units(path.as_ref(), data, &env),
-                Err(err @ CrontabFileError { kind: CrontabFileErrorKind::Io(_), .. }) => log!(logger, LogLevel::Warning, "error accessing file {}: {}", path.as_ref().display(), err),
-                Err(err @ CrontabFileError { kind: CrontabFileErrorKind::Parse(_), .. }) => log!(logger, LogLevel::Warning, "skipping file {} due to parsing error: {}", path.as_ref().display(), err),
-            }
-        }
-    }).unwrap_or_else(|err| {
-        log!(logger, LogLevel::Error, "error parsing file {}: {}", path.as_ref().display(), err);
-    });
-}
 
 #[inline]
 fn is_run_by_systemd() -> bool {
@@ -53,22 +30,10 @@ fn get_logger() -> AnyLogger {
     }
 }
 
-fn process_crontab_dir<T: ToCrontabEntry>(dir: &str, logger: &mut Logger) {
-    let files = walk_dir(dir).and_then(|fs| fs.map(|r| r.map(|p| p.path()))
-                                       .filter(|r| r.as_ref().map(|p| p.is_file()).unwrap_or(true))
-                                       .collect::<Result<Vec<PathBuf>, _>>());
-    match files {
-        Err(err) => log!(logger, LogLevel::Error, "error processing directory {}: {}", dir, err),
-        Ok(files) => for file in files {
-            process_crontab_file::<T, _>(file, logger as &mut Logger);
-        }
-    }
-}
-
 fn main() {
-    let user_thread = spawn(|| process_crontab_dir::<UserCrontabEntry>(USERS_CRONTAB_DIR, &mut get_logger()));
-    let system_thread = spawn(|| process_crontab_dir::<SystemCrontabEntry>(SYSTEM_CRONTAB_DIR, &mut get_logger()));
-    let anacron_thread = spawn(|| process_crontab_file::<AnacrontabEntry, _>(ANACRONTAB_FILE, &mut get_logger()));
+    let user_thread = spawn(|| process::process_crontab_dir::<UserCrontabEntry>(USERS_CRONTAB_DIR, &mut get_logger()));
+    let system_thread = spawn(|| process::process_crontab_dir::<SystemCrontabEntry>(SYSTEM_CRONTAB_DIR, &mut get_logger()));
+    let anacron_thread = spawn(|| process::process_crontab_file::<AnacrontabEntry, _>(ANACRONTAB_FILE, &mut get_logger()));
 
     let _ = user_thread.join();
     let _ = system_thread.join();
---
+++ src/process.rs
@@ -0,0 +1,42 @@
+use std::convert::AsRef;
+use std::fs::{walk_dir, PathExt};
+use std::path::{Path, PathBuf};
+use std::collections::BTreeMap;
+
+use cronparse::{CrontabFile, CrontabFileError, CrontabFileErrorKind};
+use cronparse::crontab::{EnvVarEntry, CrontabEntry, ToCrontabEntry};
+use cronparse::log::{Logger, LogLevel};
+
+pub fn process_crontab_dir<T: ToCrontabEntry>(dir: &str, logger: &mut Logger) {
+    let files = walk_dir(dir).and_then(|fs| fs.map(|r| r.map(|p| p.path()))
+                                       .filter(|r| r.as_ref().map(|p| p.is_file()).unwrap_or(true))
+                                       .collect::<Result<Vec<PathBuf>, _>>());
+    match files {
+        Err(err) => log!(logger, LogLevel::Error, "error processing directory {}: {}", dir, err),
+        Ok(files) => for file in files {
+            process_crontab_file::<T, _>(file, logger as &mut Logger);
+        }
+    }
+}
+
+
+pub fn process_crontab_file<T: ToCrontabEntry, P: AsRef<Path>>(path: P, logger: &mut Logger) {
+    CrontabFile::<T>::new(path.as_ref()).map(|crontab| {
+        let mut env = BTreeMap::new();
+        for entry in crontab {
+            match entry {
+                Ok(CrontabEntry::EnvVar(EnvVarEntry(name, value))) => { env.insert(name, value); },
+                Ok(data) => generate_systemd_units(path.as_ref(), data, &env),
+                Err(err @ CrontabFileError { kind: CrontabFileErrorKind::Io(_), .. }) => log!(logger, LogLevel::Warning, "error accessing file {}: {}", path.as_ref().display(), err),
+                Err(err @ CrontabFileError { kind: CrontabFileErrorKind::Parse(_), .. }) => log!(logger, LogLevel::Warning, "skipping file {} due to parsing error: {}", path.as_ref().display(), err),
+            }
+        }
+    }).unwrap_or_else(|err| {
+        log!(logger, LogLevel::Error, "error parsing file {}: {}", path.as_ref().display(), err);
+    });
+}
+
+fn generate_systemd_units(path: &Path, entry: CrontabEntry, env: &BTreeMap<String, String>) {
+    println!("{} => {:?}, {:?}", path.display(), entry, env);
+}
+
