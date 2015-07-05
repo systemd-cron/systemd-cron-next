3204bf57dbb332ff2ba0b658945a8d0b80aa3d32
Konstantin Stepanov <me@kstep.me>
2015-07-06 00:28:57 +0300

    closes #21: make sure /var is mounted, rerun generator if necessary after mount

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,10 @@ extern crate kernlog;
 
 use std::thread::spawn;
 use std::env;
+use std::fs::{File, PathExt, create_dir_all};
+use std::os::unix::fs::symlink;
+use std::io::Write;
+use std::path::Path;
 
 use cronparse::crontab::{UserCrontabEntry, SystemCrontabEntry, AnacrontabEntry};
 
@@ -24,6 +28,14 @@ static SYSTEM_CRONTAB_DIR: &'static str = "/etc/cron.d";  // SystemCrontabEntry
 static ANACRONTAB_FILE: &'static str = "/etc/anacrontab";  // AnacrontabEntry
 static REBOOT_FILE: &'static str = "/run/crond.reboot";
 
+macro_rules! try_ {
+    ($exp:expr) => {
+        match $exp {
+            Ok(value) => value,
+            Err(err) => { error!("{}", err); return; }
+        }
+    }
+}
 
 fn main() {
     kernlog::init().unwrap();
@@ -38,9 +50,31 @@ fn main() {
 
     let s = dest_dir.clone();
     let user_thread = spawn(|| {
-        process::process_crontab_dir::<UserCrontabEntry, _>(USERS_CRONTAB_DIR, s);
-        if let Err(err) = ::std::fs::File::create(REBOOT_FILE) {
-            warn!("error creating lock file {}: {}", REBOOT_FILE, err);
+        if Path::new(USERS_CRONTAB_DIR).is_dir() {
+            process::process_crontab_dir::<UserCrontabEntry, _>(USERS_CRONTAB_DIR, s);
+            if let Err(err) = File::create(REBOOT_FILE) {
+                warn!("error creating lock file {}: {}", REBOOT_FILE, err);
+            }
+        } else {
+            let cron_after_var_unit_path = Path::new(&*s).join("cron-after-var.service");
+            {
+                let mut cron_after_var_unit_file = try_!(File::create(&cron_after_var_unit_path));
+                try_!(writeln!(cron_after_var_unit_file, r###"[Unit]
+Description=Rerun systemd-crontab-generator because /var is a separate mount
+Documentation=man:systemd.cron(7)
+After=cron.target
+ConditionDirectoryNotEmpty={statedir}
+
+[Service]
+Type=oneshot
+ExecStart=/bin/sh -c "{bindir}/systemctl daemon-reload ; {bindir}/systemctl try-restart cron.target""###,
+                    statedir = USERS_CRONTAB_DIR,
+                    bindir = "/usr/bin"));
+            }
+
+            let multiuser_wants_path = Path::new(&*s).join("multi-user.target.wants");
+            try_!(create_dir_all(&multiuser_wants_path));
+            try_!(symlink(cron_after_var_unit_path, multiuser_wants_path.join("cron-after-var.service")));
         }
     });
 
