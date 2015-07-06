3e39c10f698a26b8a759f65347a1849559bdb434
Konstantin Stepanov <me@kstep.me>
2015-07-06 08:02:25 +0300

    add mail-on-failure helper

--- Makefile
+++ Makefile
@@ -1,17 +1,30 @@
 PREFIX := /usr
 
-target/release/systemd-crontab-generator target/release/boot-delay: src/*.rs src/bin/*.rs
+target/release/boot-delay: src/bin/boot-delay.rs
 	cargo build --release
 
-target/debug/systemd-crontab-generator target/debug/boot-delay:
+target/release/mail-on-failure: src/bin/mail-on-failure.rs
+	cargo build --release
+
+target/release/systemd-crontab-generator: src/*.rs
+	cargo build --release
+
+target/debug/boot-delay: src/bin/boot-delay.rs
 	cargo build
 
-release: target/release/systemd-crontab-generator target/release/boot-delay
+target/debug/mail-on-failure: src/bin/mail-on-failure.rs
+	cargo build
 
-build: target/build/systemd-crontab-generator target/build/boot-delay
+target/debug/systemd-crontab-generator:
+	cargo build
+
+release: target/release/systemd-crontab-generator target/release/boot-delay target/release/mail-on-failure
+
+build: target/debug/systemd-crontab-generator target/debug/boot-delay target/debug/mail-on-failure
 
 install: release
 	install --mode=0755 --strip -D target/release/systemd-crontab-generator ${PREFIX}/lib/systemd/system-generators/systemd-crontab-generator
+	install --mode=0755 --strip -D target/release/mail-on-failure ${PREFIX}/bin/mail-on-failure
 	install --mode=0755 --strip -D target/release/boot-delay ${PREFIX}/bin/boot-delay
 	install --mode=0644 -D units/cron.target ${PREFIX}/lib/systemd/system/cron.target
 
---
+++ src/bin/mail-on-failure.rs
@@ -0,0 +1,97 @@
+use std::env;
+use std::process::{Command, Stdio};
+use std::io::Write;
+
+fn get_systemd_unit_property(unit: &str, prop: &str) -> String {
+    String::from_utf8_lossy(&Command::new("systemctl")
+                            .arg("show")
+                            .arg(unit)
+                            .arg("--property")
+                            .arg(prop)
+                            .output()
+                            .unwrap()
+                            .stdout[prop.len() + 1..])
+        .trim_right_matches('\n')
+        .to_owned()
+}
+
+fn main() {
+    let unit = match env::args().nth(1) {
+        Some(unit) => unit,
+        None => {
+            println!("Usage: mail-on-failure <unit>");
+            return;
+        }
+    };
+
+    let mut user = get_systemd_unit_property(&*unit, "User");
+    if user.len() == 0 {
+        user = "root".to_owned();
+    }
+
+
+    let job_env = get_systemd_unit_property(&*unit, "Environment");
+    for pair in job_env.split(' ') {
+        let mut p = pair.splitn(2, '=');
+        if let (Some(name), Some(value)) = (p.next(), p.next()) {
+            if name == "MAILTO" {
+                user = value.to_owned();
+                break;
+            }
+        }
+    }
+
+    if user.len() == 0 {
+        return;
+    }
+
+    let mut hostname = String::from_utf8_lossy(&Command::new("uname")
+        .arg("-n")
+        .output()
+        .unwrap()
+        .stdout[..])
+        .trim_right_matches('\n')
+        .to_owned();
+
+    if hostname.len() == 0 {
+        hostname = "localhost".to_owned();
+    }
+
+    let mut head = String::new();
+    head.push_str("From: root (systemd-cron)\nTo: ");
+    head.push_str(&*user);
+    head.push_str("\nSubject: [");
+    head.push_str(&*hostname);
+    head.push_str("] job ");
+    head.push_str(&*unit);
+    head.push_str(r###" failed
+MIME-Version: 1.0
+Content-Type: text/plain; charset=UTF-8
+Content-Transfer-Encoding: 8bit
+Auto-Submitted: auto-generated
+
+"###);
+
+    let status = Command::new("systemctl")
+        .arg("status")
+        .arg(&*unit)
+        .output()
+        .unwrap();
+
+    let mut mailer = Command::new("sendmail")
+        .arg("-i")
+        .arg("-B8BITMIME")
+        .arg(&*user)
+        .stdin(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    println!("{}", head);
+    println!("{}", String::from_utf8_lossy(&*status.stdout));
+    if let Some(ref mut stdin) = mailer.stdin {
+        let _ = stdin.write_all(head.as_bytes()).and_then(|_|
+                stdin.write_all(&*status.stdout));
+    }
+
+    mailer.wait().unwrap();
+}
