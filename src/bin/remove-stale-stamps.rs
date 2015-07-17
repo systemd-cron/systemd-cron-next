a6be5f85ba699d81e9aa4332e25d5611bfc29cef
Konstantin Stepanov <me@kstep.me>
2015-07-17 04:02:58 +0300

    add remove-stale-stamps helper script

---
+++ src/bin/remove-stale-stamps.rs
@@ -0,0 +1,49 @@
+extern crate time;
+extern crate glob;
+
+use std::fs::{metadata, remove_file};
+use std::path::{Path, PathBuf};
+use std::os::unix::fs::MetadataExt;
+use std::collections::BTreeSet;
+
+use time::{Duration, get_time};
+use glob::glob;
+
+static KNOWN_STAMPS: [&'static str; 6] = [
+    "/var/lib/systemd/timers/stamp-cronjob-daily.timer",
+    "/var/lib/systemd/timers/stamp-cronjob-weekly.timer",
+    "/var/lib/systemd/timers/stamp-cronjob-monthly.timer",
+    "/var/lib/systemd/timers/stamp-cronjob-quarterly.timer",
+    "/var/lib/systemd/timers/stamp-cronjob-semi-annually.timer",
+    "/var/lib/systemd/timers/stamp-cronjob-yearly.timer"
+];
+
+static ACTUAL_STAMPS_GLOB: &'static str = "/var/lib/systemd/timers/stamp-cronjob-*.timer";
+static TIMER_STAMPS_GLOB: &'static str = "/run/systemd/generator/cronjob-*.timer";
+
+fn cleanup<P: AsRef<Path>, I: IntoIterator<Item=P>>(iter: I) {
+    let ten_days_ago = get_time() - Duration::days(10);
+    for stamp in iter {
+        if let Ok(meta) = metadata(&stamp) {
+            if meta.mtime() < ten_days_ago.sec {
+                let _ = remove_file(&stamp);
+            }
+        }
+    }
+}
+
+fn main() {
+    let stale_stamps =
+        &(&glob(ACTUAL_STAMPS_GLOB).unwrap()
+            .flat_map(Result::into_iter)
+            .collect::<BTreeSet<_>>() -
+        &glob(TIMER_STAMPS_GLOB).unwrap()
+            .flat_map(Result::into_iter)
+            .map(|s| PathBuf::from(s.to_string_lossy().replace("/run/systemd/generator/cronjob-", "/var/lib/systemd/timers/stamp-cronjob-")))
+            .collect::<BTreeSet<_>>()) -
+        &KNOWN_STAMPS.iter()
+            .map(PathBuf::from)
+            .collect::<BTreeSet<_>>();
+
+    cleanup(&stale_stamps);
+}
